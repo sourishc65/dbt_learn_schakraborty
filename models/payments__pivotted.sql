5b68eefbb79bb3a7a4fa6aa249bf47e80aa93dab
Sourish Chakraborty <sourishc65@gmail.com>
2021-01-15 15:25:13 +0000

    test

--- models/payments__pivotted.sql
+++ models/payments__pivotted.sql
@@ -1,3 +1,8 @@
+-- Things to figure out:
+-- trailing comma at the end (DONE!)
+-- select distinct payment method somehow (DONE!)
+-- whitespace clean up
+{%- set payment_methods = dbt_utils.get_column_values(table=ref('stg_payments'), column='payment_method', default=[]) %}
 with
 payments as (
     select * from {{ ref('stg_payments') }}
@@ -5,11 +10,13 @@ payments as (
 pivoted as (
     select 
         order_id,
-        {% for payment_method in ['bank_transfer','coupon','credit_card','gift_card'] %}
+        {% for payment_method in payment_methods %}
         sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_amount
+        {% if not loop.last %}
+        ,
+        {% endif %}
         {% endfor %}
     from payments
     group by 1
 )
-
 select * from pivoted
